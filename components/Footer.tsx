46800d24cabbf0384164c54e5ee7f7c628a0ccb0
Neha kumari <nehakumarisaa@gmail.com>
2021-06-20 12:20:21 +0530

    Removing "hello world"

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,6 @@ export const Footer: React.FC<{
 
   return (
     <footer className={styles.footer}>
-      hello world
       <div className={styles.copyright}>Copyright 2021 {config.author}</div>
 
       {hasMounted ? (
