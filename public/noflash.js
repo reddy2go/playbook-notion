0666c9d677c6dc17ef80befcdc307df8a69aac5f
Neha kumari <nehakumarisaa@gmail.com>
2021-06-22 20:26:27 +0530

    Update noflash.js

--- public/noflash.js
+++ public/noflash.js
@@ -11,7 +11,7 @@
   s.src="https://client.crisp.chat/l.js";
   s.async=1;
   d.getElementsByTagName("head")[0].appendChild(s);
-  (function prepareFrame() {
+  /*(function prepareFrame() {
       var ifrm = document.createElement("iframe");
       ifrm.setAttribute("src", "https://nehafa3.substack.com/embed");
       ifrm.style.width = "640px";
@@ -19,7 +19,7 @@
       ifrm.style.border="1px solid #EEE";
       ifrm.style.background="white";
       document.body.appendChild(ifrm);
-  })();
+  })();*/
   //<iframe style="border:1px solid #EEE; background:white;" frameborder="0" scrolling="no"></iframe>
   // Change these if you use something different in your hook.
   var storageKey = 'darkMode'
